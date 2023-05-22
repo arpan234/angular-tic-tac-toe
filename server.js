ac830ca70b484ee83af4d3c4271e12ecb4ed0b7c
Arpan Patel <arpan.patel1@farmersinsurance.com>
2023-05-21 19:46:26 -0700

    added heroku config

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/tic-tac-toe'));
+app.get('/*', function(req,res)
+{
+  res.sendFile(path.join(__dirname+'/dist/tic-tac-toe/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
